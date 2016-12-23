6bc9a6a09226b51df7df76fd0cf49ba5693a7e39
Lieby Cardoso <liebycardoso@gmail.com>
2016-12-23 20:34:45 -0200

    Add files via upload

--- Problemchars.py
+++ Problemchars.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec 22 15:03:23 2016
@@ -5,25 +6,36 @@ Created on Thu Dec 22 15:03:23 2016
 @author: MasterLieby
 """
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
 import pprint
 import re
 """
-  "lower", for tags that contain only lowercase letters and are valid,
-  "lower_colon", for otherwise valid tags with a colon in their names,
-  "problemchars", for tags with problematic characters, and
-  "other", for other tags that do not fall into the other three categories.
+  "lower" => tags com letras minúsculas
+  "lower_colon" => tags que tem dois pontos
+  "problemchars" => Caracteres problemáticos, inclusive newline, return e tab
+  "other" => Todos caracteres que não foram enquadrados acima
 """
 
 
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
-
+tag_problematica = []
 
 def key_type(element, keys):
+    """
+    Para cada chave "K", verifica e conta o total de caracteres problematicos, minusculos e outros.
+    Imprime as tags com caracteres problemáticos.
+
+    Args:
+        element: Um node do arquivo XML
+        keys: Dicionário a ser preenchido
+    Returns:
+        Retorna um dicionário com o total de tags com caracteres minúsculos, 
+        minúsculos com :, problemáticos e todos os outros.
+    """
+    
+    
     if element.tag == "tag":
         k = element.attrib['k'] 
         if lower.search(k):
@@ -32,13 +44,24 @@ def key_type(element, keys):
             keys['lower_colon']+=1
         elif problemchars.search(k):
             keys['problemchars'] +=1
-            print k
+            tag_problematica.append(k)            
         else: 
-            keys['other']+=1
+            keys['other']+=1            
         
     return keys
 
 def process_map(filename):
+    """
+    Cria o dicionário Keys com as chaves para identificar o total de tags em cada situação.
+    Percorre todo o arquivo XML, chamando a função Key_type para cada node da estrutura de dados
+
+    Args:
+        filename: nome do arquivo a ser processado.
+    Returns:
+        Retorna um dicionário com o total de tags com caracteres minúsculos, 
+        minúsculos com :, problemáticos e todos os outros.
+    """
+    
     keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
     for _, element in ET.iterparse(filename):
         keys = key_type(element, keys)
@@ -46,12 +69,10 @@ def process_map(filename):
     return keys
 
 def test():
-    # You can use another testfile 'map.osm' to look at your solution
-    # Note that the assertion below will be incorrect then.
-    # Note as well that the test function here is only used in the Test Run;
-    # when you submit, your code will be checked against a different dataset.
-    keys = process_map('C:/Nanodegree/MongoDb/Trab_final/sample_BH.osm')
+    
+    keys = process_map('C:/Nanodegree/MongoDb/Trab_final/map_BH.osm')
     pprint.pprint(keys)
+    pprint.pprint(tag_problematica)
     
 if __name__ == "__main__":
     test()
