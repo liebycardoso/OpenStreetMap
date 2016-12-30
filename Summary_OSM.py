a11ec96fe856a4f81625bd8dbf6434d847e4a9fb
Lieby Cardoso <liebycardoso@gmail.com>
2016-12-30 11:40:14 -0200

    Add files via upload

--- Summary_OSM.py
+++ Summary_OSM.py
@@ -5,18 +5,10 @@ Created on Sat Dec 24 21:05:47 2016
 
 @author: MasterLieby
 """
-
-
 """
-Your task is to use the iterative parsing to process the map file and
-find out not only what tags are there, but also how many, to get the
-feeling on how much of which data you can expect to have in the map.
-Fill out the count_tags function. It should return a dictionary with the 
-tag name as the key and number of times this tag can be encountered in 
-the map as value.
-
-Note that your code will be tested with a different data file than the 'example.osm'
+Este arquivo contém os métodos usados pela função shape_elemente()
 """
+
 import xml.etree.cElementTree as ET
 import re
 import json
